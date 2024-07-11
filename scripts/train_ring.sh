85451499dafa050e7e0f0b518e567efc85b2147f
Hank <hanklin@mit.com>
2024-07-11 06:57:52 -0400

    add acceleration to training

---
+++ scripts/train_ring.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#SBATCH -n 2 --gres=gpu:volta:1 -o results/training/behavior_net/ring_speed_acc.log-%j
+
+source /etc/profile
+source activate NNDE
+
+python3 main_training.py --config ./configs/ring_behavior_net_training_all.yml --experiment-name ring_speed_acc
