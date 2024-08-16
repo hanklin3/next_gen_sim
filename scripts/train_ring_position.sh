e714f39a8ed403aa99f0a8d4d154c98483e0fea5
Hank <hanklin@mit.com>
2024-08-16 11:12:09 -0400

    add pythonpath

--- scripts/train_ring_position.sh
+++ scripts/train_ring_position.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-#SBATCH -n 4 --gres=gpu:volta:1 -o results/training/behavior_net/ring_position.log-%j
+#SBATCH -n 4 --gres=gpu:volta:1 -o results/training/behavior_net/ring_position_close_loop.log-%j
 
 source /etc/profile
 source activate NNDE
 
+export PYTHONPATH=$PYTHONPATH:/home/gridsan/thlin/cameraculture_shared/hanklin/next_gen_sim/
+
 # python3 main_training.py --config ./configs/ring_behavior_net_training_position.yml --experiment-name ring_position_test
 
-python3 main_training.py --config ./configs/ring_behavior_net_training_position.yml --experiment-name ring_position_test
+python3 main_training.py --config ./configs/ring_behavior_net_training_position.yml --experiment-name ring_position_close_loop_8-16
